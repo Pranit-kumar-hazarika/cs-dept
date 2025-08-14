936f88648f4c048f4cfa59489a42201fadad3249
pranitttt1 <pranitkumarhazarika09@gmail.com>
2025-08-14 20:45:18 +0530

    updated the reload

--- js/contact.js
+++ js/contact.js
@@ -30,7 +30,7 @@ function toggleMenu() {
     loader.style.opacity = '0';
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 500);
+    }, 100);
   });
 
 document.addEventListener("DOMContentLoaded", function () {
--- script.js
+++ script.js
@@ -35,6 +35,6 @@ function toggleMenu() {
     loader.style.opacity = '0';
     setTimeout(() => {
       loader.style.display = 'none';
-    }, 500);
+    }, 10);
   });
 
