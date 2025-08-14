8babdbf3ddd6ef0de1640b09d2065e03b2f79885
pranitttt1 <pranitkumarhazarika09@gmail.com>
2025-08-14 19:17:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ setInterval(() => {
   slides.forEach(slide => slide.classList.remove('active'));
   index = (index + 1) % slides.length;
   slides[index].classList.add('active');
-}, 2000);
+}, 3000);
 function toggleMenu() {
   const menu = document.getElementById('mobileMenu');
   menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
